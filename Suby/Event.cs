a1d64de77f44d21eab20d0964b20ba2e5cd7a060
Nikolay Lebedev <nklbdev@gmail.com>
2018-06-08 02:04:52 +0500

    add generics up to 16 parameters

--- Suby/Event.cs
+++ Suby/Event.cs
@@ -61,4 +61,114 @@ namespace Suby
         
         public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5));
     }
+    
+    public class Event<T1, T2, T3, T4, T5, T6>: EventBase<Subscription<T1, T2, T3, T4, T5, T6>>, IEvent<T1, T2, T3, T4, T5, T6>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7>>, IEvent<T1, T2, T3, T4, T5, T6, T7>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8, v9));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15));
+    }
+    
+    public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>: EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>>, IEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
+    {
+        public Event() : base(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(null, null, null)) {}
+        
+        public IDisposable Subscribe(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> handler) =>
+            AddSubscription(new Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(null, this, handler));
+        
+        public void Raise(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16) => ForAllActive(s => s.Notify(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16));
+    }
 }
--- Suby/Subscription.cs
+++ Suby/Subscription.cs
@@ -106,4 +106,224 @@ namespace Suby
 
         public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5) => _handler(v1, v2, v3, v4, v5);
     }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6>> e,
+            Action<T1, T2, T3, T4, T5, T6> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6) => _handler(v1, v2, v3, v4, v5, v6);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7) => _handler(v1, v2, v3, v4, v5, v6, v7);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8) => _handler(v1, v2, v3, v4, v5, v6, v7, v8);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9) => _handler(v1, v2, v3, v4, v5, v6, v7, v8, v9);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10) => _handler(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11) => _handler(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12) => _handler(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13) => _handler(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14) => _handler(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15) => _handler(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15);
+    }
+    
+    public class Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>: SubscriptionBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>>
+    {
+        private readonly EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>> _event;
+        private readonly Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> _handler;
+
+        public Subscription(
+            Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> previous,
+            EventBase<Subscription<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>> e,
+            Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> handler)
+            : base(previous)
+        {
+            _event = e;
+            _handler = handler;
+        }
+
+        public override void Dispose() => _event.DisposeSubscription(this);
+
+        public void Notify(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16) => _handler(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16);
+    }
 }
